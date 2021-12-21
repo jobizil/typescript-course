6d11a00c46c09305b68ca9836cc72b59dedd7be2
Ugbem Job <ugbemjob57@gmail.com>
2021-12-21 15:17:31 +0100

    Added sorting implementation

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log("Hi ");
+// Bubble sort is like a double nested for loop.
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    let leftHand = this.collection[j];
+                    let rightHand = this.collection[j + 1];
+                    this.collection[j] = rightHand;
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hi ");
+// Bubble sort is like a double nested for loop.
+class Sorter {
+	constructor(public collection: number[]) {}
+	sort(): void {
+		const { length } = this.collection;
+		for (let i = 0; i < length; i++) {
+			for (let j = 0; j < length - i - 1; j++) {
+				if (this.collection[j] > this.collection[j + 1]) {
+					let leftHand = this.collection[j];
+					let rightHand = this.collection[j + 1];
+					this.collection[j] = rightHand;
+					this.collection[j + 1] = leftHand;
+				}
+			}
+		}
+	}
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
