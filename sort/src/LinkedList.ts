e8a149a6344ee3f269766fe8bdb2635991c45337
Ugbem Job <ugbemjob57@gmail.com>
2021-12-22 23:03:10 +0100

    Solving a linked list

---
+++ sort/src/LinkedList.ts
@@ -0,0 +1,75 @@
+class Node {
+	next: Node | null = null;
+	constructor(public data: number) {}
+}
+
+export class LinkedList {
+	head: Node | null = null;
+	add(data: number): void {
+		const node = new Node(data);
+
+		// Set head to current node to head it does not exist
+		if (!this.head) {
+			this.head = node;
+			return;
+		}
+		let tail = this.head;
+		while (tail.next) {
+			tail = tail.next;
+		}
+		tail.next = node;
+	}
+	get length(): number {
+		if (!this.head) {
+			return 0;
+		}
+		let node = this.head;
+		let length = 1;
+		while (node.next) {
+			length++;
+			node = node.next;
+		}
+		return length;
+	}
+	at(index: number): Node {
+		if (!this.head) {
+			throw new Error("Invalid index");
+		}
+		let counter = 0;
+		let node: Node | null = this.head;
+		while (node) {
+			if (counter == index) {
+				return node;
+			}
+			counter++;
+			node = node.next;
+		}
+		throw new Error("Index out of bound");
+	}
+
+	compare(leftIndex: number, rightIndex: number): boolean {
+		if (!this.head) {
+			throw new Error("List is empty");
+		}
+		return this.at(leftIndex).data > this.at(rightIndex).data;
+	}
+	swap(leftIndex: number, rightIndex: number): void {
+		const leftNode = this.at(leftIndex);
+		const rightNode = this.at(rightIndex);
+
+		const leftHand = leftNode.data;
+		leftNode.data = rightNode.data;
+		rightNode.data = leftHand;
+	}
+
+	print(): void {
+		if (!this.head) {
+			return;
+		}
+		let node: Node | null = this.head;
+		while (node) {
+			console.log(node.data);
+			node = node.next;
+		}
+	}
+}
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,9 +1,21 @@
 // import { NumbersCollection } from "./NumbersCollection";
-import { CharactersCollection } from "./CharactersCollection";
+// import { CharactersCollection } from "./CharactersCollection";
+
+import { LinkedList } from "./LinkedList";
+
 import { Sorter } from "./Sorter";
-// const numbersCollection = new NumbersCollection([10, -5, 2, -99, 0]);
-const charactersCollection = new CharactersCollection("asdfgA");
-const sorter = new Sorter(charactersCollection);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
+// const numbersCollection = new NumbersCollection([10, -5, 2, -99, 0]);
+// const charactersCollection = new CharactersCollection("ape");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
 // console.log(numbersCollection.data);
