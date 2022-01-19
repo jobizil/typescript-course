85688ddbacad0422d1f1c80a7093dd28aadb26a5
Ugbem Job <ugbemjob57@gmail.com>
2022-01-19 14:02:36 +0100

    Commented out matchReader file

--- stats/src/inheritance/MatchReader.ts
+++ stats/src/inheritance/MatchReader.ts
@@ -1,18 +1,19 @@
-// import { CsvFileReader } from "./CsvFileReader";
-// import { dateStringToDate } from "./util";
-// import { MatchResult } from "./MatchResult";
-// // Define a tuple as a new type
-// type MatchData = [Date, String, String, Number, Number, MatchResult, String];
-// export class MatchReader extends CsvFileReader<MatchData> {
-// 	mapRow(row: string[]): MatchData {
-// 		return [
-// 			dateStringToDate(row[0]),
-// 			row[1],
-// 			row[2],
-// 			parseInt(row[3]),
-// 			parseInt(row[4]),
-// 			row[5] as MatchResult, //Type Assertion
-// 			row[6],
-// 		];
-// 	}
-// }
+/* import { CsvFileReader } from "./CsvFileReader";
+import { dateStringToDate } from "./util";
+import { MatchResult } from "./MatchResult";
+// Define a tuple as a new type
+type MatchData = [Date, String, String, Number, Number, MatchResult, String];
+export class MatchReader extends CsvFileReader<MatchData> {
+	mapRow(row: string[]): MatchData {
+		return [
+			dateStringToDate(row[0]),
+			row[1],
+			row[2],
+			parseInt(row[3]),
+			parseInt(row[4]),
+			row[5] as MatchResult, //Type Assertion
+			row[6],
+		];
+	}
+}
+ */
